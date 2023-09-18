30076398a3975077eac109a8e7c5e978fbb59de7
PaulinaIvan <paulina.ivanauskaite1@gmail.com>
2023-09-18 13:34:11 -0700

    added comments

--- XAM/Models/Exam.cs
+++ XAM/Models/Exam.cs
@@ -3,6 +3,8 @@ public class Exam
     public string Name { get; set; }
     public DateTime Date { get; set; }
 
+    //Each exam will also have flashcards data, but for now name and date should be alright
+
     public Exam(string name, DateTime date)
     {
         Name = name;

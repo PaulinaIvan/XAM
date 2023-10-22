adf36b81faf1f58f6f52dace6dd44475b337bef9
Dominykas Dobkevičius <144054096+DominykasDobkevicius@users.noreply.github.com>
2023-10-22 10:10:52 +0300

    Updated the string extension

--- XAM/Models/Extensions.cs
+++ XAM/Models/Extensions.cs
@@ -3,15 +3,6 @@ public static class Extensions
     // Returns true if string contains only letters and spaces.
     public static bool IsMadeOfLettersNumbersAndSpaces(this string str)
     {
-        if(str == null)
-            return false;
-
-        foreach(char c in str)
-        {
-            if(!char.IsLetter(c) && !char.IsNumber(c) && c != ' ')
-                return false;
-        }
-
-        return true;
+        return !string.IsNullOrEmpty(str) && str.All(c => char.IsLetterOrDigit(c) || c == ' ');
     }
 }

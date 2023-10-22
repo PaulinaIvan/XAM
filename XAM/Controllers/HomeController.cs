51b51bb8e93b7ce06e7559f6fdcb2ee5fa69842c
Dominykas Dobkevičius <144054096+DominykasDobkevicius@users.noreply.github.com>
2023-10-22 10:13:25 +0300

    Removed old requirements

--- XAM/Controllers/HomeController.cs
+++ XAM/Controllers/HomeController.cs
@@ -8,14 +8,11 @@ namespace XAM.Controllers;
 public class HomeController : Controller
 {
     // Requirements not achieved:
-    // 1.4 Creating and using your own enum;
-    // 7. Create and use at least 1 generic type;
-    // 8. Boxing and Unboxing;
 
     private readonly ILogger<HomeController> _logger;
     private readonly ExamDataSingleton _dataHolder;
 
-    public record ErrorRecord(string ErrorCode, string ErrorMessage); // 1.3 Creating and using your own record;
+    public record ErrorRecord(string ErrorCode, string ErrorMessage);
 
     public HomeController(ILogger<HomeController> logger, ExamDataSingleton dataHolder)
     {
@@ -40,10 +37,8 @@ public class HomeController : Controller
 
     public IActionResult FetchExams()
     {
-        // 9.1. LINQ to Objects usage (methods);
         List<Exam> correctlyNamedExams = _dataHolder.Exams.Where(exam => exam.Name.IsMadeOfLettersNumbersAndSpaces()).ToList();
 
-        // 9.2. LINQ to Objects usage (queries);
         // (Same thing as above, but with queries)
         // List<Exam> correctlyNamedExams = (from exam in _dataHolder.Exams where exam.Name.IsMadeOfLettersNumbersAndSpaces() select exam).ToList();
 
@@ -52,7 +47,7 @@ public class HomeController : Controller
 
     public IActionResult CreateExam(string name, string date)
     {
-        if (!name.IsMadeOfLettersNumbersAndSpaces()) // 4. Extension method usage.
+        if (!name.IsMadeOfLettersNumbersAndSpaces())
         {
             string error = "Invalid exam name.";
             Console.WriteLine(error);
@@ -74,7 +69,7 @@ public class HomeController : Controller
             ErrorRecord errorResponse = CreateErrorResponse("BadDate", error);
             return Json(errorResponse);
         }
-        Exam newExam = new(date: parsedDate, name: name); // 3.1. Named argument usage;
+        Exam newExam = new(date: parsedDate, name: name);
 
         _dataHolder.Exams.Add(newExam);
 
@@ -86,7 +81,7 @@ public class HomeController : Controller
         return Json(result);
     }
 
-    ErrorRecord CreateErrorResponse(string ErrorCode, string ErrorMessage = "Unknown error.") // 3.2. Optional argument usage;
+    ErrorRecord CreateErrorResponse(string ErrorCode, string ErrorMessage = "Unknown error.")
     {
         ErrorRecord ErrorResponse = new(ErrorCode, ErrorMessage);
         return ErrorResponse;
@@ -176,7 +171,7 @@ public class HomeController : Controller
             try
             {
                 List<Exam> uniqueExams = new();
-                using (var reader = new StreamReader(file.OpenReadStream())) // 6. Reading from a file using a stream.
+                using (var reader = new StreamReader(file.OpenReadStream()))
                 {
                     var fileContent = reader.ReadToEnd();
                     List<Exam>? examsFromFile = JsonSerializer.Deserialize<List<Exam>>(fileContent,
@@ -186,7 +181,7 @@ public class HomeController : Controller
                         });
 
                     if (examsFromFile != null)
-                        foreach (Exam examFromFile in examsFromFile) // 5. Iterating through collection the right way.
+                        foreach (Exam examFromFile in examsFromFile)
                         {
                             if (_dataHolder.Exams.Find(exam => exam.Name == examFromFile.Name) == null)
                                 uniqueExams.Add(examFromFile);

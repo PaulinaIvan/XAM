367fb25ff38d9635bf24483566c86170c35f110c
PaulinaIvan <paulina.ivanauskaite1@gmail.com>
2023-10-31 13:23:20 -0700

    cocktail qualifications assessment

--- XAM/wwwroot/js/cocktail.js
+++ XAM/wwwroot/js/cocktail.js
@@ -1,13 +1,33 @@
 document.getElementById('cocktailButton').addEventListener('click', function () {
-    fetch('/Home/GetRandomCocktail')
-        .then(response => response.json())
-        .then(data => {
-            if (data && data.drinks && data.drinks.length > 0) {
-                const cocktailName = data.drinks[0].strDrink;
-                document.getElementById('cocktailName').textContent = `Cocktail Name: ${cocktailName}`;
-            } else {
-                console.error('Invalid data format received from the server.');
+    fetch(`/Home/FetchStatistics`)
+    .then(response => response.json())
+    .then(data => {
+        if (data !== null)
+        {
+            if (data.lifetimeExams > 0) {
+                fetch('/Home/GetRandomCocktail')
+                .then(response => response.json())
+                .then(data => {
+                    if (data && data.drinks && data.drinks.length > 0) {
+                        const cocktailName = data.drinks[0].strDrink;
+                        document.getElementById('cocktailName').textContent = `Cocktail Name: ${cocktailName}`;
+                    } else {
+                        console.error('Invalid data format received from the server.');
+                    }
+                })
+                .catch(error => console.error('Error:', error));
             }
-        })
-        .catch(error => console.error('Error:', error));
+            else {
+                document.getElementById('cocktailName').textContent = `You haven't worked hard enough to deserve a cocktail yet!`;
+            }
+        }
+        else
+        {
+            console.log('Invalid data format received from the server.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
 });

4cbc6bcffb045a4a5f1ab779a53f980fb8e53c8c
Juras Ezerskis <ezerskis.juras@gmail.com>
2023-11-20 20:30:35 +0200

    More tests
    Split our tests into multiple files and tried to cover all of the models files, its not 100% but its in the green. :)

---
+++ MyIntegrationTests/ErrorViewModelTests.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using XAM.Models;
+
+namespace ErrorViewModelTests
+{
+    public class ErrorViewModelTests
+    {
+        [Fact]
+        public void RequestId_SetAndGet_ReturnsCorrectValue()
+        {
+            // Arrange
+            var model = new ErrorViewModel();
+            var requestId = "123";
+
+            // Act
+            model.RequestId = requestId;
+
+            // Assert
+            Assert.Equal(requestId, model.RequestId);
+        }
+
+        [Theory]
+        [InlineData(null, false)]
+        [InlineData("", false)]
+        [InlineData("123", true)]
+        public void ShowRequestId_ReturnsCorrectValue(string requestId, bool expected)
+        {
+            // Arrange
+            var model = new ErrorViewModel { RequestId = requestId };
+
+            // Act
+            var result = model.ShowRequestId;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
